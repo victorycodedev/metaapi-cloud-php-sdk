55280376f668053885b6d02e805736bc82c4bcb3
Victory <45511695+Victory33404@users.noreply.github.com>
2023-04-02 21:09:50 +0100

    Add support to get account metrics and open trades

---
+++ src/Resources/Metastats/Metrics.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Victorycodedev\MetaapiCloudPhpSdk\Resources\Metastats;
+
+trait Metrics
+{
+    /*
+    *   Calculates and returns a MetaApi account metrics. This API call is billable
+    */
+    public function metrics(string $accountId, bool $includeOpenPositions = false): array|string
+    {
+        return $this->http->get("{$this->baseUrl}/users/current/accounts/{$accountId}/metrics?includeOpenPositions={$includeOpenPositions}");
+    }
+
+    /*
+    *   Returns open trades for MetaApi account. This API call is not billable
+    */
+    public function openTrades(string $accountId): array|string
+    {
+        return $this->http->get("{$this->baseUrl}/users/current/accounts/{$accountId}/open-trades");
+    }
+}
